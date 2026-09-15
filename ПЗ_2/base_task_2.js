081e7d9958f245a96d625ed62aac1f1aa03c6543
sakitari <daniilpolyvacgu@gmail.com>
2026-09-15 22:57:35 +0300

    upd: выполнено задание 1.2

--- ПЗ_2/base_task_2.js
+++ ПЗ_2/base_task_2.js
@@ -1,5 +1,32 @@
 // Задание 1.1 Работа с арифметическими операторами и преобразованием типов
 const currentYear = new Date().getFullYear()
+
 let birthYear = Number(prompt('Введите год рождения:'))
+let guests = Number(prompt('Введите количество гостей, которых планируете взять с собой (максимум 4):'))
+let maxPeople = 5
+
 let userAge = currentYear - birthYear
-console.log(`Ваш возраст: ${userAge} лет`)
+let allPeoples = 1 + guests
+
+console.log(
+`Ваш возраст: ${userAge} лет.
+Общее количество человек: ${allPeoples}.`
+)
+
+//Задание 1.2 Работа с операторами сравнения и логическими операторами
+if (userAge >= 18 && allPeoples <= maxPeople) {
+    console.log(
+    `Пользователь совершеннолетний и общее количество человек не превышает установленный лимит: ${maxPeople}.
+    Регистрация разрешена.`
+    )
+} else if (allPeoples > maxPeople) {
+    console.log(`
+    Превышено допустимое число человек.
+    Регистрация не разрешена.`)
+} else {
+    console.log(`
+    Пользователь не достиг совершеннолетия.
+    Регистрация не разрешена`)
+}
+
+console.log('Код выполнен без ошибок')
